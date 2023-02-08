5fc7801775d9bcd2d121d65f12fb7d563fe98be3
RobertoSimonini <robertosimonini1@gmail.com>
2023-02-08 17:27:17 +0100

    added back to form button

--- password.php
+++ password.php
@@ -7,10 +7,12 @@ $password = $_SESSION['pass'];
 ?>
 
 
-<body>
-    <h1 class="text-center py-5">
+<body class="text-center py-5">
+    <h1>
         La password è <?= $password ?>
     </h1>
+
+    <a href="http://localhost/php-strong-password-generator" class="btn btn-secondary">Torna al form</a>
 </body>
 
 </html>
