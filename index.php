4cd6f58d0a15dccf18c16dad9457982932f6aaed
RobertoSimonini <robertosimonini1@gmail.com>
2023-02-08 17:24:06 +0100

    Created a new Head file

--- index.php
+++ index.php
@@ -24,6 +24,7 @@ Milestone 5 (BONUS): Aggiungere la validazione
 
 // Prendo la funzione da un file esterno 
 include './functions.php';
+include './head.php';
 
 // Starto la sessione e invio la password tramite $_SESSION
 session_start();
@@ -33,21 +34,6 @@ $_SESSION['pass'] = randomPassword();
 if (!empty($_SESSION['pass']))
     header('Location: password.php');
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <!-- Bootstrap -->
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous' />
-
-</head>
 
 <body>
     <form action="#" class="w-50 mx-auto py-5" method="get">
